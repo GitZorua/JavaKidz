9d8485089b4dbb60378fb0941543ee46a8dcdc81
Zorua <zoruadoesthings@gmail.com>
2022-02-01 17:36:11 +0900

    Lots of work done, after corrupting my VS project 3 times.

--- JavaKidz/Form1.Designer.cs
+++ JavaKidz/Form1.Designer.cs
@@ -30,15 +30,18 @@ namespace JavaKidz
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(JavaKidz));
             this.installButton = new System.Windows.Forms.Button();
             this.progressBar = new System.Windows.Forms.ProgressBar();
-            this.outputBox = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.apiButton = new System.Windows.Forms.CheckBox();
+            this.dOptions_fabricAPI = new System.Windows.Forms.CheckBox();
             this.loaderButton = new System.Windows.Forms.CheckBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.bgTooltip = new System.Windows.Forms.ToolTip(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.outputBox = new System.Windows.Forms.RichTextBox();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // installButton
@@ -53,21 +56,11 @@ namespace JavaKidz
             // 
             // progressBar
             // 
+            this.progressBar.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
             this.progressBar.Location = new System.Drawing.Point(8, 95);
             this.progressBar.Name = "progressBar";
             this.progressBar.Size = new System.Drawing.Size(639, 14);
             this.progressBar.TabIndex = 1;
-            // 
-            // outputBox
-            // 
-            this.outputBox.BackColor = System.Drawing.SystemColors.WindowText;
-            this.outputBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.outputBox.FormattingEnabled = true;
-            this.outputBox.Location = new System.Drawing.Point(8, 115);
-            this.outputBox.Name = "outputBox";
-            this.outputBox.SelectionMode = System.Windows.Forms.SelectionMode.None;
-            this.outputBox.Size = new System.Drawing.Size(639, 121);
-            this.outputBox.TabIndex = 2;
             // 
             // label1
             // 
@@ -79,17 +72,17 @@ namespace JavaKidz
             this.label1.TabIndex = 3;
             this.label1.Text = "Download Options";
             // 
-            // apiButton
+            // dOptions_fabricAPI
             // 
-            this.apiButton.AutoSize = true;
-            this.apiButton.Checked = true;
-            this.apiButton.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.apiButton.Location = new System.Drawing.Point(7, 53);
-            this.apiButton.Name = "apiButton";
-            this.apiButton.Size = new System.Drawing.Size(75, 17);
-            this.apiButton.TabIndex = 4;
-            this.apiButton.Text = "Fabric API";
-            this.apiButton.UseVisualStyleBackColor = true;
+            this.dOptions_fabricAPI.AutoSize = true;
+            this.dOptions_fabricAPI.Checked = true;
+            this.dOptions_fabricAPI.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.dOptions_fabricAPI.Location = new System.Drawing.Point(7, 53);
+            this.dOptions_fabricAPI.Name = "dOptions_fabricAPI";
+            this.dOptions_fabricAPI.Size = new System.Drawing.Size(75, 17);
+            this.dOptions_fabricAPI.TabIndex = 4;
+            this.dOptions_fabricAPI.Text = "Fabric API";
+            this.dOptions_fabricAPI.UseVisualStyleBackColor = true;
             // 
             // loaderButton
             // 
@@ -105,7 +98,7 @@ namespace JavaKidz
             // 
             this.panel1.Controls.Add(this.loaderButton);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.apiButton);
+            this.panel1.Controls.Add(this.dOptions_fabricAPI);
             this.panel1.Location = new System.Drawing.Point(8, 202);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(639, 96);
@@ -115,21 +108,47 @@ namespace JavaKidz
             // 
             this.bgTooltip.ToolTipTitle = "Credit";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-7, -43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(674, 360);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.bgTooltip.SetToolTip(this.pictureBox1, " Animated Background by Артём Саркисян");
+            // 
+            // outputBox
+            // 
+            this.outputBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.outputBox.ForeColor = System.Drawing.Color.Lime;
+            this.outputBox.Location = new System.Drawing.Point(8, 115);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ReadOnly = true;
+            this.outputBox.Size = new System.Drawing.Size(639, 121);
+            this.outputBox.TabIndex = 9;
+            this.outputBox.Text = "";
+            this.outputBox.TextChanged += new System.EventHandler(this.outputBox_TextChanged);
+            // 
             // JavaKidz
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
             this.ClientSize = new System.Drawing.Size(659, 310);
             this.Controls.Add(this.outputBox);
             this.Controls.Add(this.progressBar);
             this.Controls.Add(this.installButton);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "JavaKidz";
             this.ShowIcon = false;
             this.Text = "JavaKidz";
             this.Load += new System.EventHandler(this.JavaKidz_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -138,12 +157,13 @@ namespace JavaKidz
 
         private System.Windows.Forms.Button installButton;
         private System.Windows.Forms.ProgressBar progressBar;
-        private System.Windows.Forms.ListBox outputBox;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.CheckBox apiButton;
+        private System.Windows.Forms.CheckBox dOptions_fabricAPI;
         private System.Windows.Forms.CheckBox loaderButton;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.ToolTip bgTooltip;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox outputBox;
     }
 }
 
--- JavaKidz/Form1.cs
+++ JavaKidz/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Net;
 using System.IO;
+using System.Threading;
 
 namespace JavaKidz
 {
@@ -17,14 +18,27 @@ namespace JavaKidz
         public JavaKidz()
         {
             InitializeComponent();
-        }
 
+        }
         private void button1_Click(object sender, EventArgs e)
         {
+            Console.WriteLine("Starting new thread...");
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                downloadManager();
+            }).Start();
+        }
+
+        private void downloadManager()
+        {
             //Get updated links
-            outputBox.Items.Add("Grabbing updated download links...");
+            Console.WriteLine("Grabbing updated download links...");
             string optifineLink = "";
             string portalLink = "";
+            string optifabricLink = "";
+            string packLink = "";
+            string fabricAPILink = "";
             try
             {
 
@@ -33,8 +47,9 @@ namespace JavaKidz
                 Stream dataStream = response.GetResponseStream();
                 StreamReader reader = new StreamReader(dataStream);
                 optifineLink = reader.ReadToEnd();
+                Console.WriteLine("- Got up-to-date link for OptiFine.");
             }
-            catch { outputBox.Items.Add("Cannot grab download link for OptiFine, please contact the JKInstaller developer..."); }
+            catch { Console.WriteLine("Cannot grab download link for OptiFine, please contact the JKInstaller developer..."); }
             try
             {
 
@@ -43,14 +58,50 @@ namespace JavaKidz
                 Stream dataStream = response.GetResponseStream();
                 StreamReader reader = new StreamReader(dataStream);
                 portalLink = reader.ReadToEnd();
+                Console.WriteLine("- Got up-to-date link for JKPortal.");
             }
-            catch { outputBox.Items.Add("Cannot grab download link for JKPortal, please contact the JKInstaller developer..."); }
+            catch { Console.WriteLine("Cannot grab download link for JKPortal, please contact the JKInstaller developer..."); }
+            try
+            {
+
+                WebRequest request = WebRequest.Create("https://raw.githubusercontent.com/RoNubThe3rd/JavaKidz/master/JavaKidz/DLinks/optifabric.txt");
+                WebResponse response = request.GetResponse();
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                optifabricLink = reader.ReadToEnd();
+                Console.WriteLine("- Got up-to-date link for OptiFabric.");
+            }
+            catch { Console.WriteLine("Cannot grab download link for OptiFabric, please contact the JKInstaller developer..."); }
+            try
+            {
+
+                WebRequest request = WebRequest.Create("https://raw.githubusercontent.com/RoNubThe3rd/JavaKidz/master/JavaKidz/DLinks/pack.txt");
+                WebResponse response = request.GetResponse();
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                packLink = reader.ReadToEnd();
+                Console.WriteLine("- Got up-to-date link for the resource pack.");
+            }
+            catch { Console.WriteLine("Cannot grab download link for JavaKidz resource pack, please contact the JKInstaller developer..."); Console.WriteLine("I blame Frow ^"); }
+            try
+            {
+
+                WebRequest request = WebRequest.Create("https://raw.githubusercontent.com/RoNubThe3rd/JavaKidz/master/JavaKidz/DLinks/fabricAPI.txt");
+                WebResponse response = request.GetResponse();
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                optifabricLink = reader.ReadToEnd();
+                Console.WriteLine("- Got up-to-date link for Fabric API.");
+            }
+            catch { Console.WriteLine("Cannot grab download link for FabricAPI, please contact the JKInstaller developer..."); }
+
             string modsFolder = Environment.GetEnvironmentVariable("USERPROFILE") + @"\" + @"AppData\Roaming\.minecraft\mods";
             string resourceFolder = Environment.GetEnvironmentVariable("USERPROFILE") + @"\" + @"AppData\Roaming\.minecraft\resourcepacks";
             //OptiFine
             using (WebClient wc = new WebClient())
             {
-                wc.DownloadProgressChanged += ProgressChanged;
+                //wc.DownloadProgressChanged += ProgressChanged;
+                Console.WriteLine("Downloading OptiFine...");
                 try
                 {
                     wc.DownloadFile( //OptiFine Download
@@ -58,78 +109,162 @@ namespace JavaKidz
                         new System.Uri(optifineLink),
                         // Param2 = Path to save
                         modsFolder + @"\OptiFine 1.17.1 HD U H1.jar"
-                    ); outputBox.Items.Add("Completed installation of OptiFine...");
+                    ); Console.WriteLine("Completed installation of OptiFine...");
                 }
-                catch { outputBox.Items.Add("Could not install OptiFine..."); }
+                catch { Console.WriteLine("> Could not install OptiFine..."); }
             }
             //JKPortal
             using (WebClient wc = new WebClient())
             {
-                wc.DownloadProgressChanged += ProgressChanged;
+                //wc.DownloadProgressChanged += ProgressChanged;
+                Console.WriteLine("Downloading JKPortal...");
                 try
                 {
                     wc.DownloadFile( //JKPortal Download
                         new System.Uri(portalLink),
                         modsFolder + @"\JKPortal 1.0.7.jar"
-                    ); outputBox.Items.Add("Completed installation of JKPortal...");
+                    ); Console.WriteLine("Completed installation of JKPortal...");
                 }
-                catch { outputBox.Items.Add("Could not install JKPortal..."); }
+                catch { Console.WriteLine("> Could not install JKPortal..."); }
 
             }
             //OptiFabric
             using (WebClient wc = new WebClient())
             {
-                wc.DownloadProgressChanged += ProgressChanged;
+                //wc.DownloadProgressChanged += ProgressChanged;
+                Console.WriteLine("Downloading OptiFabric...");
                 try
                 {
                     wc.DownloadFile( //OptiFabric Download
-                        new System.Uri("https://www.curseforge.com/minecraft/mc-mods/optifabric/download/3422922/file"),
+                        new System.Uri(optifabricLink),
                         modsFolder + @"\OptiFabric 1.11.20.jar"
-                    ); outputBox.Items.Add("Completed installation of OptiFabric");
+                    ); Console.WriteLine("Completed installation of OptiFabric");
                 }
                 catch
                 {
-                    outputBox.Items.Add("Could not install OptiFabric...");
-                    outputBox.Items.Add("CurseForge may have blocked the download since I am a 'bot', attempting to download from backup link...");
+                    Console.WriteLine("> Could not install OptiFabric...");
+                    Console.WriteLine("CurseForge may have blocked the download since I am a 'bot', attempting to download from backup link...");
                     try
                     {
                         wc.DownloadFile( //OptiFabric Download
-                            new System.Uri("https://download1585.mediafire.com/hgetjchkqwqg/xt4zso1ury8th15/optifabric-1.11.20.jar"),
+                            new System.Uri("https://download1585.mediafire.com/hgetjchkqwqg/xt4zso1ury8th15/optifabric-1.11.20.jar"), //this'll eventually change and not work anymore, f
                             modsFolder + @"\OptiFabric 1.11.20.jar"
-                        ); outputBox.Items.Add("Completed installation of OptiFabric from backup MediaFire link!");
+                        ); Console.WriteLine("Completed installation of OptiFabric from backup MediaFire link!");
                     }
                     catch
                     {
-                        outputBox.Items.Add("Could not install OptiFabric from backup link.");
-                        outputBox.Items.Add("You will have to download and install the file manually.");
+                        Console.WriteLine("> Could not install OptiFabric from backup link.");
+                        Console.WriteLine("You will have to download and install the file manually.");
                     }
                 }
             }
             //Resource Pack
             using (WebClient wc = new WebClient())
             {
-                wc.DownloadProgressChanged += ProgressChanged;
+                //wc.DownloadProgressChanged += ProgressChanged;
+                Console.WriteLine("Downloading JavaKidz Resource Pack...");
                 try
                 {
-                    wc.DownloadFileAsync( //Resource Pack Download
-                    new System.Uri("https://github.com/RoNubThe3rd/jk/raw/ResourcePack/Javakidz_Test_1.0.5.zip"),
+                    wc.DownloadFile( //Resource Pack Download
+                    new System.Uri(packLink),
                     resourceFolder + @"\§6Java§8Kidz §7Resources.zip"
-                ); outputBox.Items.Add("Completed installation of resource pack...");
+                ); Console.WriteLine("Completed installation of resource pack...");
                 }
-                catch { outputBox.Items.Add("Could not install resource pack..."); }
+                catch { Console.WriteLine("> Could not install resource pack..."); }
 
             }
-            outputBox.Items.Add("Completed installations!");
+            Console.WriteLine("Completed installations!");
+
+            //FabricAPI
+            if (dOptions_fabricAPI.Checked)
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    //wc.DownloadProgressChanged += ProgressChanged;
+                    Console.WriteLine("Downloading Fabric API...");
+                    try
+                    {
+                        wc.DownloadFile( //Fabric API Download
+                        new System.Uri(fabricAPILink),
+                        modsFolder + @"\Fabric API.jar"
+                    ); Console.WriteLine("Completed installation of Fabric API...");
+                    }
+                    catch { Console.WriteLine("> Could not install Fabric API..."); }
+
+                }
+            }
+            Console.WriteLine("Completed installations!");
         }
 
-        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
-        {
-            progressBar.Value = e.ProgressPercentage;
-        }
+
+        //private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        //{
+        //    Console.WriteLine(e.ProgressPercentage+"%");
+        //}
 
         private void JavaKidz_Load(object sender, EventArgs e)
         {
+            Console.SetOut(new RichTextBoxWriter(outputBox));
+            Console.WriteLine("> > Welcome to the JavaKidz Installer! < <");
+        }
 
+        private void outputBox_TextChanged(object sender, EventArgs e)
+        {
+            outputBox.SelectionStart = outputBox.Text.Length;
+            outputBox.ScrollToCaret();
+        }
+
+        public delegate void StringArgReturningVoidDelegate(string text);
+        public class RichTextBoxWriter : TextWriter
+        {
+            private readonly RichTextBox _richTextBox;
+            public RichTextBoxWriter(RichTextBox richTexttbox)
+            {
+                _richTextBox = richTexttbox;
+            }
+
+            public override void Write(char value)
+            {
+                SetText(value.ToString());
+            }
+
+            public override void Write(string value)
+            {
+                SetText(value);
+            }
+
+            public override void WriteLine(char value)
+            {
+                SetText(value + Environment.NewLine);
+            }
+
+            public override void WriteLine(string value)
+            {
+                SetText(value + Environment.NewLine);
+            }
+
+            public override Encoding Encoding => Encoding.ASCII;
+
+            //Write to your UI object in thread safe way:
+            private void SetText(string text)
+            {
+                // InvokeRequired required compares the thread ID of the  
+                // calling thread to the thread ID of the creating thread.  
+                // If these threads are different, it returns true.  
+                if (_richTextBox.InvokeRequired)
+                {
+                    var d = new StringArgReturningVoidDelegate(SetText);
+                    _richTextBox.Invoke(d, text);
+                }
+                else
+                {
+                    _richTextBox.Text += text;
+                }
+            }
+        }
+
+        private void outputBox_TextChanged_1(object sender, EventArgs e)
+        {
         }
     }
 }
